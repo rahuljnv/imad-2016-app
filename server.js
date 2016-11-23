30541e31f20bd46c93a9c92e5da556ec65eac7f8
rahuljnv <rahulmishrajnv@gmail.com>
2016-11-23 21:06:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,84 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articles = {
+ 'article-one': {
+    title: 'article One | rahul mishra',
+    heading: "about me!!",
+  
+    content: `<p>
+               hello! this is rahul mishra.I'm persuing btech in SRM Uniersity.this is my first web app related to modern aticles.
+               Here in this website you are going to view many categorised articles.Even you can post any modern articles by logging to our site.
+            </p>`
+},
+ 'article-two': {
+    title: "TECHNOLGY | rahul mishra",
+    heading: "WELCOME TO OUT PAGE ",
+    date: "sep 6,2015",
+    content: `<p>
+                WELCOME TO OUT TECHNOLGY PAGE 
+            </p>`
+}
+};
+function createtemplate(data){
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+    
+var htmltemplate = `
+                <html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+         <link href="/ui/style.css" rel="stylesheet" />
+        
+    </head>
+    <body>
+        <div class="container">
+        <div>
+            <a href="/">home</a>
+        </div>
+        <hr/>
+        <h3>
+            ${heading}
+        </h3>
+        <div>
+           ${date}
+        </div>
+        <div>
+            ${content}
+        </div>
+        </div>
+    </body>
+</html>`;
+return htmltemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
+app.get('/counter',function(req,res){
+ counter= counter+1;
+ res.send(counter.toString());
+});
+
+app.get('/:articleName',function(req,res){
+    var articleName = req.params.articleName;
+    res.send(createtemplate(articles[articleName]));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
